16b3b52cf365005650440d1902c3616c9adf9151
Lou <verdun.lou@gmail.com>
2019-11-07 10:08:54 +0100

    AnimationWorklet

--- animation/animation.js
+++ animation/animation.js
@@ -1,5 +1,11 @@
-class animClass {
-    
+function remap(minIn, maxIn, minOut, maxOut, v) {
+    return (v - minIn) / (maxIn - minIn) * (maxOut - minOut) + minOut;
 }
 
-registerAnimation('my-anim', animClass);
+class AllRound {
+    animate(currentTime, effect) {
+        effect.localTime = currentTime; // Timeline we want to use
+    }
+}
+
+registerAnimator('allround', AllRound);
---
+++ animation/main.js
@@ -0,0 +1,50 @@
+// Document timeline
+// requestAnimationFrame(function raf(now) {
+//     console.log(document.timeline);
+
+//     requestAnimationFrame(raf)
+// });
+
+const init = async () => {
+    if ('animationWorklet' in CSS) {
+
+        await CSS.animationWorklet.addModule('animation.js');
+
+        new WorkletAnimation(
+            'allround',
+            new KeyframeEffect(
+                document.querySelector('.element'),
+                [
+                    {
+                        transform: 'translate3d(0, 0, 0)'
+                    },
+                    {
+                        transform: 'translate3d(calc(100vw - 250px), 0, 0)'
+                    },
+                    {
+                        transform: 'translate3d(calc(100vw - 250px), calc(100vh - 250px), 0)'
+                    },
+                    {
+                        transform: 'translate3d(0, calc(100vh - 250px), 0)'
+                    },
+                    {
+                        transform: 'translate3d(0, 0, 0)'
+                    }
+                ],
+                {
+                    duration: 4000,
+                    iterations: Number.POSITIVE_INFINITY
+                }
+            ),
+            document.timeline
+            // new ScrollTimeline({
+            //     scrollSource: document.querySelector('.scroller'),
+            //     orientation: "vertical", // "horizontal" or "vertical".
+            //     timeRange: 4000
+            // })
+        )
+        .play();
+    }
+}
+
+init();
